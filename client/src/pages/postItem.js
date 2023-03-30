73eba7df320cb6c854c9ed1dc85187f8c022b745
Carlo Caballero <carlopcaballero@gmail.com>
2023-03-30 19:05:08 -0400

    Basic frontend routing

--- client/src/pages/postItem.js
+++ client/src/pages/postItem.js
@@ -1,14 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
-const postItem = () => {
+const PostItem = () => {
     const [openAuctions, addNewAuction] = useState({});
 
 
     return (
         <>
+        <h1>Post Item</h1>
+
+        <div className="item-display">
+
+        <div className="description-field">
+            <p>Describe the item you are selling:</p>
+            <input className="item-description" type="text"></input>
+        </div>
+        <div className="price-field">
+            <p>What should be the starting bid for this item?</p>
+            <input type="item-price"></input>
+        </div>
         
+        <div className="postType">
+        <input type="checkbox" id="markAsSale" name="markAsSale" value="forSale"/>
+        <label for="sale-item">I want to put this item up for sale.</label>
+        <input type="checkbox" id="markAsAuction" name="markAsAuction" value="forAuction"/>
+        <label for="sale-item">I want to put this item up for Auction.</label>
+        </div>
+        </div> 
         </>
     );
 };
 
-export default postItem;
+export default PostItem;
--- client/src/pages/viewItem.js
+++ client/src/pages/viewItem.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
-const viewItem = () => {
+const ViewItem = () => {
     const [openAuctions, addNewAuction] = useState({});
 
 
     return (
         <>
-        
+        <h1>View Item</h1>
+        <div className="item-display">
+            <img src="">
+
+            </img>
+            <div className="description-field">
+            <p>Item description</p>            
+            </div>
+        <div className="price-field">
+            <h4>Item Price</h4>
+            </div>
+        </div>
         </>
     );
 };
 
-export default viewItem;
+export default ViewItem;
